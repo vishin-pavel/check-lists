3c799bb07a6c9bfcd2ccf070ae4e9138dfd49833
Ikaev Evgeniy <evgeniy.ikaev@livingcore.ru>
2017-05-22 18:03:21 +0300

    HomePage версия 1.0

---
+++ web/assets/common/js/helper.js
@@ -0,0 +1,16 @@
+//Функция выравнивания дивов
+function setEqualHeight(columns) {
+    var tallestcolumn = 0;
+    columns.each(
+        function () {
+            currentHeight = $(this).height();
+            if (currentHeight > tallestcolumn) {
+                tallestcolumn = currentHeight;
+            }
+        }
+    );
+    columns.height(tallestcolumn);
+}
+$(document).ready(function () {
+    setEqualHeight($(".info-title > div"));
+});
