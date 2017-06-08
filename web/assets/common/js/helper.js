e92d01e7024c9b27bb4a71cf58b6959b24ce9004
igor <igorlivcore@gmail.com>
2017-06-08 14:02:30 +0300

    общие правки + переработка главного меню

--- web/assets/common/js/helper.js
+++ web/assets/common/js/helper.js
@@ -14,3 +14,35 @@ function setEqualHeight(columns) {
 $(document).ready(function () {
     setEqualHeight($(".info-title > div"));
 });
+
+
+//удаление прикрепленного документа
+var fileBlockConstructor = function($el){
+    this.$el = $el;
+    this.deleteFile = function(file){
+        swal({
+                title: "Вы уверены?",
+                text: "Файл будет удален без возможности восстановления",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonClass: "btn-danger",
+                confirmButtonText: "Удалить",
+                cancelButtonText: "Отмена",
+                closeOnConfirm: false,
+            },
+            function (isConfirm) {
+                if (isConfirm) {
+                    swal({
+                        title: "Файл удален",
+                        type: "success",
+                        confirmButtonClass: "btn-success"
+                    });
+                    $(file).parent().remove();
+                }
+            }
+        );
+    };
+    $(".file>i", $el).click(this, function (event){
+        event.data.deleteFile($(event.currentTarget))
+    }, this);
+};
